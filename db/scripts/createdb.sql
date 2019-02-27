163137f5b6dc1db27a54354cb72fc84a86572f71
Zachary Metz <zachary.metz@ucalgary.ca>
2019-02-27 09:40:26 -0700

    Create createdb.sql

---
+++ db/scripts/createdb.sql
@@ -0,0 +1,32 @@
+Create Table account(
+	accountid serial PRIMARY KEY,
+	publickey text not null,
+	privatekey text not null,
+	created_at timestamp with time zone not null
+);
+
+
+Create Table nickname(
+	nicknameid serial PRIMARY KEY,
+	accountid int not null REFERENCES account(accountid),
+	name text not null,
+	color int default 0, /*	the hedidecimal value for the color of the user's name  */
+	created_at timestamp with time zone not null
+	
+);
+
+Create Table chatroom(
+	chatroomid serial PRIMARY KEY,
+	created_by int REFERENCES account(accountid),
+	name text not null,
+	hash text not null,
+	created_at timestamp with time zone not null
+);
+
+Create Table message(
+	messageid serial PRIMARY KEY,
+	created_by int REFERENCES account(accountid),
+	created_in int REFERENCES chatroom(chatroomid),
+	message text not null,
+	created_at timestamp with time zone not null
+);
